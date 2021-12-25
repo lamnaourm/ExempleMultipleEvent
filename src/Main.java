2cec6b88d24160269acbac6b3dac720ab161ee0a
lamnaourm <mohammed.lamnaour@gmail.com>
2021-12-25 20:39:25 +0100

    First commit

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,19 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.JTextField;
 
 public class Main extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txt_nb1;
+	private JTextField txt_nb2;
+	private JTextField txt_res;
 
 	/**
 	 * Launch the application.
@@ -34,7 +43,7 @@ public class Main extends JFrame {
 	 */
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 540, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -42,32 +51,90 @@ public class Main extends JFrame {
 		
 		ActionListener e = (event) -> {
 			JButton b = (JButton)event.getSource();
+			double res = 0;
+			double val1 = Double.parseDouble(txt_nb1.getText());
+			double val2 = Double.parseDouble(txt_nb2.getText());
 			
 			switch (b.getName()) {
-			case "b1":JOptionPane.showMessageDialog(null, "Button 1");break;
-			case "b2":JOptionPane.showMessageDialog(null, "Button 2");break;
-			case "b3":JOptionPane.showMessageDialog(null, "Button 3");break;
-			default:
+			case "bplus":res= val1+val2; break;
+			case "bmoins": res= val1-val2; break;
+			case "bprod":res= val1*val2; break;
+			case "bdiv":
+				if(val2==0) {
+					JOptionPane.showMessageDialog(null, "Division par zero");
+				}else 
+					res= val1/val2; 
 				break;
+
 			}
+			
+			txt_res.setText(String.valueOf(res));
+			
+			
 		};
 		
-		JButton button = new JButton("1");
-		button.setName("b1");
-		button.setBounds(37, 141, 89, 23);
+		JButton button = new JButton("+");
+		button.setName("bplus");
+		button.setBounds(41, 216, 89, 33);
 		contentPane.add(button);
 		button.addActionListener(e);
 		
-		JButton button_1 = new JButton("2");
-		button_1.setName("b2");
-		button_1.setBounds(136, 141, 89, 23);
+		JButton button_1 = new JButton("-");
+		button_1.setName("bmoins");
+		button_1.setBounds(140, 216, 89, 33);
 		contentPane.add(button_1);
 		button_1.addActionListener(e);
 		
-		JButton button_2 = new JButton("3");
-		button_2.setName("b3");
-		button_2.setBounds(235, 141, 89, 23);
+		JButton button_2 = new JButton("*");
+		button_2.setName("bprod");
+		button_2.setBounds(239, 216, 89, 33);
 		contentPane.add(button_2);
 		button_2.addActionListener(e);
+		
+		JLabel lblNewLabel = new JLabel("Calculatrice - Version 1");
+		lblNewLabel.setBorder(new LineBorder(Color.PINK, 2));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblNewLabel.setBounds(74, 28, 334, 46);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nombre 1 : ");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(74, 104, 128, 29);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Nombre 2 : ");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1_1.setBounds(74, 144, 128, 29);
+		contentPane.add(lblNewLabel_1_1);
+		
+		txt_nb1 = new JTextField();
+		txt_nb1.setBounds(199, 110, 194, 20);
+		contentPane.add(txt_nb1);
+		txt_nb1.setColumns(10);
+		txt_nb1.setDocument(new ControleDecimal());
+		
+		txt_nb2 = new JTextField();
+		txt_nb2.setColumns(10);
+		txt_nb2.setBounds(199, 150, 194, 20);
+		contentPane.add(txt_nb2);
+		txt_nb2.setDocument(new ControleDecimal());
+		
+		JButton button_2_1 = new JButton("/");
+		button_2_1.setName("bdiv");
+		button_2_1.setBounds(338, 216, 89, 33);
+		contentPane.add(button_2_1);
+		button_2_1.addActionListener(e);
+		
+		txt_res = new JTextField();
+		txt_res.setEnabled(false);
+		txt_res.setColumns(10);
+		txt_res.setBounds(199, 283, 194, 20);
+		contentPane.add(txt_res);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Resultat :");
+		lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1_1_1.setBounds(74, 277, 128, 29);
+		contentPane.add(lblNewLabel_1_1_1);
 	}
 }
